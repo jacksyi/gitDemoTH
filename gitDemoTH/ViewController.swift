f08bcf3ecba6c03fb01b5525ec608f365a850019
Jack Yi <Jack@jacks-macbook-pro.local>
2017-05-18 01:36:42 -0700

    Added address property

--- gitDemoTH/ViewController.swift
+++ gitDemoTH/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let userName = "realUser"
+    let address = "Some Address"
 
     override func viewDidLoad() {
         super.viewDidLoad()
